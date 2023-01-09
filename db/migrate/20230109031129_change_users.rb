11fbbbd3ea934d6b5a1dac7ec2a0a7bf607c2855
Elias Tracy <elitracy@tamu.edu>
2023-01-09 13:24:04 +0000

    added new migration to drop and readd users table

---
+++ db/migrate/20230109031129_change_users.rb
@@ -0,0 +1,12 @@
+class ChangeUsers < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :users, if_exists: true
+    drop_table :user, if_exists: true
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :role
+      t.timestamps
+    end
+  end
+end
