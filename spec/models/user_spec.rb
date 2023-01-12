0b387634d9ca65ef8bbc7ad756433916e3c23795
Elias Tracy <elitracy@tamu.edu>
2023-01-12 00:58:44 +0000

    Rspec User controller tests added and passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,28 +11,28 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "update" do
+  describe 'update' do
     it "updates an existing user's name and email" do
-      user = User.create(name: "John Doe", email: "johndoe@example.com", role: "Coach")
-      user.update(name: "Jane Doe", email: "janedoe@example.com")
+      user = User.create(name: 'John Doe', email: 'johndoe@example.com', role: 'Coach')
+      user.update(name: 'Jane Doe', email: 'janedoe@example.com')
 
-      expect(user.name).to eq("Jane Doe")
-      expect(user.email).to eq("janedoe@example.com")
+      expect(user.name).to eq('Jane Doe')
+      expect(user.email).to eq('janedoe@example.com')
     end
   end
 
-  describe "destroy" do
-    it "deletes an existing user" do
-      user = User.create(name: "John Doe", email: "johndoe@example.com", role: "Coach")
+  describe 'destroy' do
+    it 'deletes an existing user' do
+      user = User.create(name: 'John Doe', email: 'johndoe@example.com', role: 'Coach')
       user.destroy
 
       expect(User.find_by(id: user.id)).to be_nil
     end
   end
 
-  describe "show" do
-    it "returns the details of an existing user" do
-      user = User.create(name: "John Doe", email: "johndoe@example.com", role: "Coach")
+  describe 'show' do
+    it 'returns the details of an existing user' do
+      user = User.create(name: 'John Doe', email: 'johndoe@example.com', role: 'Coach')
       shown_user = User.find(user.id)
 
       expect(shown_user.name).to eq(user.name)
@@ -41,10 +41,10 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "index" do
-    it "lists all existing users" do
-      user1 = User.create(name: "John Doe", email: "johndoe@example.com", role: "Coach")
-      user2 = User.create(name: "Jane Doe", email: "janedoe@example.com", role: "Admin")
+  describe 'index' do
+    it 'lists all existing users' do
+      user1 = User.create(name: 'John Doe', email: 'johndoe@example.com', role: 'Coach')
+      user2 = User.create(name: 'Jane Doe', email: 'janedoe@example.com', role: 'Admin')
       users = User.all
 
       expect(users.count).to eq(2)
