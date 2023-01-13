397476fe75e9622bad23bd0e84cf8406e88603dc
AndrewImwalle <animwalle@gmail.com>
2023-01-13 07:49:55 +0000

    fixing mailer issue in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   host = 'https://fighting-aggies-platform.herokuapp.com/' # replace with your own url
-  config.action_mailer.default_url_options({ host: })
+  config.action_mailer.default_url_options({host: host})
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
