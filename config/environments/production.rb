5c5a00cd909988c85a746f99b2b1e94e29087fc3
Elias Tracy <elitracy@tamu.edu>
2023-01-23 18:17:25 +0000

    changed mailer back to faap-staging heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
-  host = 'https://fighting-aggies-platform.herokuapp.com/' # replace with your own url
+  host = 'https://faap-staging.herokuapp.com/' # replace with your own url
   config.action_mailer.default_url_options({ host: })
 
   # SMTP settings for gmail
