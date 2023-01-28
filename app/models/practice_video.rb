7701b6a7d0650418aaa4d2c255121ddf0b7be331
AndrewImwalle <animwalle@gmail.com>
2023-01-28 02:12:15 +0000

    Set upload date to current time and fixed corresponding tests

--- app/models/practice_video.rb
+++ app/models/practice_video.rb
@@ -15,5 +15,6 @@ class PracticeVideo < ApplicationRecord
   def store_metadata
     self.filename = clip.filename
     # self.video_create_date = clip.created_at
+    self.video_upload_date = Time.now.utc.iso8601
   end
 end
