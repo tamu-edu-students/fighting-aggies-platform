f5591ad69a03e6b7fe75948d487e8418d039d46e
Philip Ritchey <philipritchey@users.noreply.github.com>
2023-02-13 17:26:15 -0600

    cleaned up setup.sh

--- setup.sh
+++ setup.sh
@@ -17,109 +17,110 @@ check_install() {
         echo -e "${RED}$cmd $bad_msg${NC}"
         return 0
     fi
-    sleep 3
+    
 }
 
 echo -e "${CYAN}Setting up... this should take 5-10 minutes${NC}"
-sleep 5
-
 echo -e "${YELLOW}Checking conflicting for conflicting yarn binary${NC}"
 echo -e "${YELLOW}Don't worry if package is not already installed${NC}"
-sleep 2
 sudo apt remove cmdtest
 
 # install apt dependencies
 echo -e "${CYAN}Installing apt dependencies${NC}"
-sleep 3
 sudo apt -y update
-sudo apt -y install git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev  ffmpeg cucumber libpq-dev pkg-config postgresql
+sudo apt -y install git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev  ffmpeg cucumber libpq-dev pkg-config
 echo -e "${GREEN}apt dependencies successfully installed${NC}"
-sleep 3
-
 
 # installs rbevn
-echo -e "${CYAN}Installing rbenv${NC}"
-sleep 3
-curl -fsSL 'https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer' | bash
-echo -e 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo -e 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
-check_install 'rbenv' 'successfully installed' 'failed to install'
+RBENV=$(which rbenv)
+if [ -z "$RBENV" ]; then
+  echo -e "${CYAN}Installing rbenv${NC}"
+  
+  curl -fsSL 'https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer' | bash
+  echo -e 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo -e 'eval "$(rbenv init -)"' >> ~/.bashrc
+  source ~/.bashrc
+  check_install 'rbenv' 'successfully installed' 'failed to install'
+else
+  echo -e "${GREEN}$(rbenv --version) is installed${NC}"
+fi
 
 # install google-chrome for cucumber testing
-echo -e "${CYAN}Installing google-chrome for testing${NC}"
-sleep 3
-wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -fy
-check_install 'google-chrome' 'successfully installed' 'failed to install'
+CHROME=$(which google-chrome)
+if [ -z "$CHROME" ]; then
+  echo -e "${CYAN}Installing google-chrome for testing${NC}"
+  
+  wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
+  sudo apt-get install -fy
+  check_install 'google-chrome' 'successfully installed' 'failed to install'
+  rm google-chrome-stable_current_amd64.deb*
+else
+  echo -e "${GREEN}$(google-chrome --version) is installed${NC}"
+fi
 
-retries=3
-while [ $retries -gt 0 ]; do
-  wget 'https://chromedriver.storage.googleapis.com/106.0.5249.61/chromedriver_linux64.zip'
-  unzip chromedriver_linux64.zip
-  sudo mv chromedriver /usr/bin/chromedriver
-  sudo chown root:root /usr/bin/chromedriver
-  sudo chmod +x /usr/bin/chromedriver
-  install_status=$(check_install 'chromedriver' 'successfully installed' 'failed to install')
-  if [ $install_status -eq 0 ]; then
-    retries=$(( retries - 1 ))
-    echo -e "${YELLOW}chromedriver failed to install trying again...${retries}/3${NC}"
-  else
-    break
-  fi
-done
-check_install 'chromedriver' 'successfully installed' 'failed to install'
+DRIVER=$(which chromedriver)
+if [ -z "$DRIVER" ]; then
+  retries=3
+  while (( retries -gt 0 )); do
+    wget 'https://chromedriver.storage.googleapis.com/106.0.5249.61/chromedriver_linux64.zip'
+    unzip chromedriver_linux64.zip
+    sudo mv chromedriver /usr/bin/chromedriver
+    sudo chown root:root /usr/bin/chromedriver
+    sudo chmod +x /usr/bin/chromedriver
+    install_status=$(check_install 'chromedriver' 'successfully installed' 'failed to install')
+    if [ "$install_status" -eq "0" ]; then
+      retries=$(( retries - 1 ))
+      echo -e "${YELLOW}chromedriver failed to install trying again...${retries}/3${NC}"
+    else
+      break
+    fi
+  done
+  check_install 'chromedriver' 'successfully installed' 'failed to install'
+  rm chromedriver_linux64.zip*
+else
+  echo -e "${GREEN}$(chromedriver --version) is installed${NC}"
+fi
 
-# install ruby
-echo -e "${CYAN}Installing Ruby${NC}"
-sleep 3
-
-retries=3
-while [ $retries -gt 0 ]; do
-  LATEST_RUBY_VERSION=$(rbenv install --list | grep -E '^[0-9]+\.[0-9]+\.[0-9]+' | tail -1)
-  rbenv install $LATEST_RUBY_VERSION -v
-  rbenv global $LATEST_RUBY_VERSION
-  rbenv local $LATEST_RUBY_VERSION
-  ruby -v
-  install_status=$(check_install 'ruby' 'successfully installed' 'failed to install')
-  if [ $install_status -eq 0 ]; then
-    retries=$(( retries - 1 ))
-    echo -e "${YELLOW}ruby failed to install trying again...${retries}/3${NC}"
-  else
-    break
-  fi
-done
-check_install 'ruby' 'successfully installed' 'failed to install'
-
-source ~/.bashrc
+RUBY=$(which ruby)
+if [ -z "$RUBY" ]; then
+  # install ruby
+  echo -e "${CYAN}Installing Ruby${NC}"
+  retries=3
+  while (( retries > 0 )); do
+    LATEST_RUBY_VERSION=$(rbenv install --list | grep -E '^[0-9]+\.[0-9]+\.[0-9]+' | tail -1)
+    rbenv install -s $LATEST_RUBY_VERSION -v
+    rbenv local $LATEST_RUBY_VERSION
+    check_install 'ruby' 'successfully installed' 'failed to install'
+    if [ "$?" -eq "0" ]; then
+      retries=$(( retries - 1 ))
+      echo -e "${YELLOW}ruby failed to install trying again...${retries}/3${NC}"
+    else
+      break
+    fi
+  done
+else
+  echo -e "${GREEN}$(ruby --version) is installed${NC}"
+fi
 
 # add extra gems
 echo -e "${CYAN}Adding gems${NC}"
-sleep 3
 gem install selenium-webdriver -v 3.142.7
 gem install rails 
 echo -e "${GREEN}gems successfully added to Gemfile${NC}"
-sleep 3
 
 # update and install all gems
 echo -e "${CYAN}Installing gems${NC}"
-sleep 3
 bundle update
 bundle install
 echo -e "${GREEN}gems successfully updated and installed${NC}"
-sleep 3
 
 echo -e "${CYAN}Installing npm${NC}"
-sleep 3
 sudo apt install -y npm
 check_install 'npm' 'successfully installed' 'failed to install'
 
-source ~/.bashrc
-
 # add npm version manager 
 echo -e "${CYAN}Installing yarn${NC}"
-sleep 3
 sudo npm install --global n
 sudo n latest
 
@@ -129,28 +130,13 @@ yarn upgrade
 yarn install
 check_install 'yarn' 'successfully installed' 'failed to install'
 
-# get newly installed commands
-source ~/.bashrc
-
-# migrate models and see dbs
+# load schema and seed db
 echo -e "${CYAN}Updating ruby models and databases${NC}"
-sleep 3
-rake db:migrate
-rake db:seed
-
-# testing 
-echo -e "${CYAN}Running unit tests${NC}"
-sleep 3
-rake cucumber
-rspec
+rails db:setup
+rails db:schema:dump
+rails db:test:load
 
 # clean up
 echo -e "${CYAN}Cleaning up${NC}"
-
-sleep 3
-rm chromedriver_linux64.zip*
-rm google-chrome-stable_current_amd64.deb*
-
 echo -e "${GREEN}All setup processes COMPLETE! ${NC}"
-
 echo -e "${CYAN}If you have an error running bin/dev, source ~/.bashrc to update your paths${NC}"
